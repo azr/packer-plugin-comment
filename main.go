c9c2d069949ef81c0f3e92052324c21346f350b5
Adrien Delorme <adrien.delorme@icloud.com>
2021-02-10 16:32:37 +0100

    use sdk and multi-plugin-binary pattern

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
+	"github.com/hashicorp/packer-plugin-sdk/version"
+)
+
+var (
+	// The main version number that is being run at the moment.
+	Version = "2.0.0"
+
+	// A pre-release marker for the Version. If this is "" (empty string)
+	// then it means that it is a final release. Otherwise, this is a pre-release
+	// such as "dev" (in development), "beta", "rc1", etc.
+	VersionPrerelease = ""
+
+	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
 )
 
 func main() {
-	server, err := plugin.Server()
-	if err != nil {
-		panic(err)
-	}
+	pps := plugin.NewSet()
 
-	server.RegisterProvisioner(new(CommentProvisioner))
-	server.Serve()
+	pps.RegisterProvisioner(plugin.DEFAULT_NAME, new(CommentProvisioner))
+	pps.SetVersion(PluginVersion)
+	err := pps.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
